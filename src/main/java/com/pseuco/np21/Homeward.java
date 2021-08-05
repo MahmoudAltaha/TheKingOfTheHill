1186c0398f50519f6c3aea177ea3cc57c897b167
Mustafa Muslim <s8mamusl@stud.uni-saarland.de>
2021-08-05 20:29:38 +0200

    Added Methods (enterTrail, enterClearing and dropFood) to the class Homeward.java

--- src/main/java/com/pseuco/np21/Homeward.java
+++ src/main/java/com/pseuco/np21/Homeward.java
@@ -2,10 +2,56 @@ package com.pseuco.np21;
 
 public class Homeward {
 
+    private final Ant ant;
 
+    /**
+     * constructor
+     * @param ant
+     */
 
+    public Homeward(Ant ant) {
+        this.ant = ant;
+    }
 
+    /**
+     * handling how to enter a Trail t.
+     *
+     * @param c     The current Clearing
+     * @param t     The target trail
+     * @return      true by successfully entering the trail.
+     * @throws      InterruptedException
+     */
 
+    public synchronized boolean enterTrail (Clearing c, Trail t) throws InterruptedException{
+        assert t != null;
+        while (!t.isSpaceLeft()){
+            wait();
+        }
+        t.enter();
+        return true;
+    }
 
+    /**
+     * handling how to enter a Clearing c.
+     *
+     * @param t      The current trail.
+     * @param c      The target Clearing.
+     * @return       true by successfully entering the Clearing.
+     * @throws       InterruptedException
+     */
+    public synchronized boolean enterClearing(Trail t, Clearing c) throws InterruptedException{
+        //TODO implement this
+        return true;
+    }
+
+    /**
+     * drop the food item into the anthill
+     * @param c
+     * @return      true by successfully dropping food
+     */
+    public synchronized boolean dropFood (Clearing c) {
+        //TODO implement this
+        return true;
+    }
 
 }
