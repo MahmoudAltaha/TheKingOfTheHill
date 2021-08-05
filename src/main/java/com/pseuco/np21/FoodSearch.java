5ea64385f22bbdf66976324571d1f46838b6fc84
Mahmoud Altaha <s8maalta@stud.uni-saarland.de>
2021-08-05 19:20:58 +0200

    adding some Methods (structure in Ant Class and in FoodSearch Class

--- src/main/java/com/pseuco/np21/FoodSearch.java
+++ src/main/java/com/pseuco/np21/FoodSearch.java
@@ -2,6 +2,104 @@ package com.pseuco.np21;
 
 public class FoodSearch {
 
+    private final Ant ant;
+
+    /**
+     * constructor for the Class
+     * @param ant
+     */
+    public FoodSearch(Ant ant) {
+        this.ant = ant;
+    }
+
+    /**
+     *this methode will be used to handle the entering to a Trail according to the behavior of an Ant.
+     *
+     * @param c    The Clearing from which the Ant comes.
+     * @param t    The Trail which the Ant is heading to.
+     * @return     true when the Ant has entered the Trail successfully.
+     * @throws InterruptedException
+     */
+    public boolean enterTrail(Clearing c, Trail t)throws InterruptedException {
+        assert t  != null ;
+        while (!t.isSpaceLeft()){
+            wait();
+        }
+        t.enter();
+        return true;
+    }
+
+    /**
+     * this methode will be used to handle the entering to a Clearing according to the behavior of an Ant.
+     *
+     * @param t   The Trail from which the Ant comes.
+     * @param c   The Clearing which the Ant is heading to .
+     * @return    true if the Ant has entered the Clearing successfully
+     * @throws InterruptedException
+     */
+    public boolean enterClearing(Trail t,Clearing c)throws InterruptedException {
+        while (!c.isSpaceLeft()){
+            wait(ant.disguise());
+        }
+        /*ToDo check whether the ant has left the wait(disguise) by having killed
+            if so then terminate Now!!
+           */
+        //TODO handle entering
+        return  true;
+    }
+
+    /**
+     * this methode will be used to step back to the last Clearing from the sequence .
+     *
+     * @param c     The Clearing from which the Ant comes.
+     * @param t     The Trail which the Ant is heading to .
+     * @return      true if the Ant has entered the Trail successfully
+     * @throws InterruptedException
+     */
+    public boolean backToTrail(Clearing c, Trail t)throws InterruptedException {
+        assert t  != null ;
+        while (!t.isSpaceLeft()){
+            wait();
+        }
+        //TODO handle entering
+        return false;
+    }
+
+
+    /**
+     * this methode will be used to step back to the last Clearing from the sequence .
+     *
+     * @param t    The Trail from which the Ant comes.
+     * @param c    The Clearing which the Ant is heading to .
+     * @return     true if the Ant has entered the Clearing successfully.
+     * @throws InterruptedException
+     */
+    public boolean backTOClearing(Trail t,Clearing c)throws InterruptedException {
+        while (!c.isSpaceLeft()){
+            wait(ant.disguise());
+        }
+        /*ToDo check whether the ant has left the wait(disguise) by having killed
+            if so then terminate Now!!
+           */
+        //TODO handle entering
+        return  true;
+    }
+
+    /**
+     * this methode will be userd
+     * @param c     The current Clearing from which the Ant will pick up the Food.
+     * @return      true if the food was collected successfully.
+     */
+    public boolean pickUPFood(Clearing c){
+        //TODO handle the process of picking up the food.
+        return  false;
+    }
+
+
+
+
+
+
 
 
 }
