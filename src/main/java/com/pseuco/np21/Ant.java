1f6d1b1e28ff89fe449b5387d571722b80ebc851
Mahmoud Altaha <s8maalta@stud.uni-saarland.de>
2021-08-14 15:36:45 +0200

    removing while from run

--- src/main/java/com/pseuco/np21/Ant.java
+++ src/main/java/com/pseuco/np21/Ant.java
@@ -288,7 +288,7 @@ public class Ant extends com.pseuco.np21.shared.Ant implements Runnable {
    * Primary ant behavior.
    */
   public void run() {
-    while(!Thread.currentThread().isInterrupted()){
+
       position = world.anthill();
       recorder.spawn(this);
       //TODO CHECK, Anthill should not be added to the Sequence
@@ -322,7 +322,6 @@ public class Ant extends com.pseuco.np21.shared.Ant implements Runnable {
 
     }
 
-  }
 
 }
 
